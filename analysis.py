3ed59f4a9783c7db7213de261cd7919e23296d60
Giovanni Valentini <valentini.giovanni96@gmail.com>
2022-10-24 19:20:20 +0200

    __main__ added

--- analysis.py
+++ analysis.py
@@ -1,25 +1,29 @@
 import numpy as np
 import pandas as pd
 
-data_path = "C:/Users/valen/OneDrive/Documenti/GSQSA/tabellini/"
-output_path = "C:/Users/valen/OneDrive/Documenti/GSQSA/gsqsa_basket/output/"
+from utils import clean_stats
 
-# read number of sheets
-tabellini = pd.ExcelFile(data_path+"tabellini.xlsx")
-n_partite = len(tabellini.sheet_names)
-print(f'number of matches: {n_partite}')
+if __name__ == "__main__":
 
-# read all sheets into dict
-df_dict = pd.read_excel(data_path+"tabellini.xlsx", sheet_name=None)
+    data_path = "C:/Users/valen/OneDrive/Documenti/GSQSA/tabellini/"
+    output_path = "C:/Users/valen/OneDrive/Documenti/GSQSA/gsqsa_basket/output/"
 
-# clean df
-df_dict_cleaned = {k:clean_stats(v) for k, v in df_dict.items()}
-
-# concat tabs
-tabs = pd.concat(list(df_dict_cleaned.values()))
-
-# aggregates
-tab_agg = tabs.groupby('NOME', as_index=False).agg({'PTS':'mean','RT':'mean','AST':'mean'}).round(2)
-
-# send to csv
-tab_agg.sort_values(by='PTS', ascending=False).to_csv(output_path+"tab_agg.csv", index=False)
+    # read number of sheets
+    tabellini = pd.ExcelFile(data_path+"tabellini.xlsx")
+    n_partite = len(tabellini.sheet_names)
+    print(f'number of matches: {n_partite}')
+    
+    # read all sheets into dict
+    df_dict = pd.read_excel(data_path+"tabellini.xlsx", sheet_name=None)
+    
+    # clean df
+    df_dict_cleaned = {k:clean_stats(v) for k, v in df_dict.items()}
+    
+    # concat tabs
+    tabs = pd.concat(list(df_dict_cleaned.values()))
+    
+    # aggregates
+    tab_agg = tabs.groupby('NOME', as_index=False).agg({'PTS':'mean','RT':'mean','AST':'mean'}).round(2)
+    
+    # send to csv
+    tab_agg.sort_values(by='PTS', ascending=False).to_csv(output_path+"tab_agg.csv", index=False)
