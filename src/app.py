f862d130a98aa7324e012a19f98932aed3a00da1
Giovanni Valentini <valentini.giovanni96@gmail.com>
2022-11-12 16:18:22 +0100

    deploying app

--- src/app.py
+++ src/app.py
@@ -3,9 +3,9 @@ import pandas as pd
 import streamlit as st
 
 # read data
-data_path = "C:/Users/valen/OneDrive/Documenti/GSQSA/gsqsa_basket/output/"
-github_data_path = "Giovalentini/gsqsa_basket/tree/main/output/"
-df = pd.read_csv(github_data_path + 'tabs.csv')
+#data_path = "C:/Users/valen/OneDrive/Documenti/GSQSA/gsqsa_basket/output/"
+github_data_path = "https://raw.githubusercontent.com/Giovalentini/gsqsa_basket/main/output/tabs.csv"
+df = pd.read_csv(github_data_path)
 
 # build app
 st.write("""
