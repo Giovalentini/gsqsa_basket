aaefebceb5cf970467ebc90ec57b5ceae09988f9
Giovanni Valentini <valentini.giovanni96@gmail.com>
2022-11-19 15:11:04 +0100

    trying aggrid...

--- src/app/pages/1_Team_Stats.py
+++ src/app/pages/1_Team_Stats.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import streamlit as st
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 # read data
 #data_path = "C:/Users/valen/OneDrive/Documenti/GSQSA/gsqsa_basket/output/"
@@ -13,6 +14,29 @@ st.write("""
 GSQSA Team Stats
 """,unsafe_allow_html=True)
 
-cols_to_round = ['FG%','3P%','2P%','FT%']
+#cols_to_round = ['FG%','3P%','2P%','FT%']
+#AgGrid(df.style.format(subset=cols_to_round, formatter="{:.2f}"))
 
-st.dataframe(df.style.format(subset=cols_to_round, formatter="{:.2f}"))
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+)
+
+df = grid_response['df']
+selected = grid_response['selected_rows'] 
+df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+#st.dataframe(df.style.format(subset=cols_to_round, formatter="{:.2f}"))
