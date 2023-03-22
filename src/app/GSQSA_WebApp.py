9b2847dc792e95fee933f3c9522ec6e0c821b243
Giovanni Valentini <valentini.giovanni96@gmail.com>
2023-03-22 01:03:42 +0100

    graphic stuff

--- src/app/GSQSA_WebApp.py
+++ src/app/GSQSA_WebApp.py
@@ -7,3 +7,7 @@ st.write("""
 """)
 
 st.sidebar.success("Select between Players or Team Stats above")
+
+# GSQSA logo
+github_data_path = "https://raw.githubusercontent.com/Giovalentini/gsqsa_basket/main/"
+st.image(github_data_path+"gsqsa_logo.png", width=300)
